277646e3252a46074c5c7b68b19e180109246a0a
Daniel Don <dandons25@gmail.com>
2020-02-21 22:24:06 +0100

    add animate scrolls to several parts of the webpage and some extra features

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", function(){
   const box = document.querySelector(".modal");
   M.Modal.init(box, {});
 })
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 80
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
