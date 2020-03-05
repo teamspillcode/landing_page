2f185215ec2a2f68f984e74615cc9aab233df1a2
Daniel Don <dandons25@gmail.com>
2020-03-05 13:34:32 +0100

    adjust affiliate section to contain a link to a google form instead - change the mailchimp link to another mailchimp link - make some other minor css fixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(){
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+      var target = $  (this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
